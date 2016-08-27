6f3535a6aca90c6aeb20db8cafcd9727694c7d2e
lizhen3250 <764482832@qq.com>
2016-08-27 23:04:57 +0800

    implemented linear search and binary search

--- Charpter02/Charpter02.UnitTest/InsertionSortTest.cs
+++ Charpter02/Charpter02.UnitTest/InsertionSortTest.cs
@@ -22,5 +22,21 @@ namespace Charpter02.UnitTest
             int[] result = InsertionSort.DescendingSort(arr);
             CollectionAssert.AreEqual(result, new int[] { 59, 58, 41, 41, 31, 26 });
         }
+
+        [TestMethod]
+        public void TestMethodLinearSearch()
+        {
+            int[] arr = { 31, 41, 59, 26, 41, 58 };
+            int result = InsertionSort.LinserSearch(arr, 41);
+            Assert.AreEqual(result, 41);
+        }
+
+        [TestMethod]
+        public void TestMethodBinarySearch()
+        {
+            int[] arr = { 31, 41, 59, 26, 41, 58 };
+            int result = InsertionSort.BinarySearch(arr, 41);
+            Assert.AreEqual(result, 41);
+        }
     }
 }
--- Charpter02/Charpter02/InsertionSort.cs
+++ Charpter02/Charpter02/InsertionSort.cs
@@ -39,5 +39,33 @@ namespace Charpter02
             }
             return arr;
         }
+
+        public static int LinserSearch(int[] arr, int value)
+        {
+            for(int i=0; i<arr.Length; i++)
+            {
+                if (arr[i] == value)
+                    return arr[i];
+            }
+            return -1;
+        }
+
+        public static int BinarySearch(int[] arr, int value)
+        {
+            int[] sortedArray = Sort(arr);
+            int start = 0;
+            int end = arr.Length - 1;
+            while(start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] == value)
+                    return arr[mid];
+                else if (arr[mid] > value)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+            return -1;
+        }
     }
 }
