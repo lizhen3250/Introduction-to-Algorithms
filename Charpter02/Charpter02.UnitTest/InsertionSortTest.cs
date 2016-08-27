ec07d9bc309a5c0923ded27b98b12ad6e37a0559
lizhen3250 <764482832@qq.com>
2016-08-27 22:53:01 +0800

    implemented insertion sort by descending order

--- Charpter02/Charpter02.UnitTest/InsertionSortTest.cs
+++ Charpter02/Charpter02.UnitTest/InsertionSortTest.cs
@@ -14,5 +14,13 @@ namespace Charpter02.UnitTest
             int[] result = InsertionSort.Sort(arr);
             CollectionAssert.AreEqual(result, new int[] { 26, 31, 41, 41, 58, 59 });
         }
+
+        [TestMethod]
+        public void TestMethodInsertionSortByDescending()
+        {
+            int[] arr = { 31, 41, 59, 26, 41, 58 };
+            int[] result = InsertionSort.DescendingSort(arr);
+            CollectionAssert.AreEqual(result, new int[] { 59, 58, 41, 41, 31, 26 });
+        }
     }
 }
--- Charpter02/Charpter02/InsertionSort.cs
+++ Charpter02/Charpter02/InsertionSort.cs
@@ -23,5 +23,21 @@ namespace Charpter02
             }
             return arr;
         }
+
+        public static int[] DescendingSort(int[] arr)
+        {
+            for(int i=1; i<arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while(j >=0 && arr[j] < key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+            return arr;
+        }
     }
 }
