cfe0228eb0a27c65a9dbeb584a0249baef1c6ecf
lizhen3250 <764482832@qq.com>
2016-08-30 01:16:05 +0800

    Implemented Merge Sort

---
+++ Charpter02/Charpter02.UnitTest/MergeSortTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Charpter02;
+
+namespace Charpter02.UnitTest
+{
+    [TestClass]
+    public class MergeSortTest
+    {
+        [TestMethod]
+        public void MergeSort()
+        {
+            int[] arr = { 31, 41, 59, 26, 41, 58 };
+            MergeSortAlgorithm.Merge(arr, 0, arr.Length);
+            CollectionAssert.AreEqual(arr, new int[] { 26, 31, 41, 41, 58, 59 });
+        }
+    }
+}
---
+++ Charpter02/Charpter02/MergeSortAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charpter02
+{
+    public class MergeSortAlgorithm
+    {
+        public static void Merge(int[] unsorted, int left, int right)
+        {
+            if (left + 1 < right)
+            {
+                int mid = (left + right) / 2;
+                Merge(unsorted, left, mid);
+                Merge(unsorted, mid, left);
+                Sort(unsorted, left, mid, right);
+            }
+        }
+        private static void Sort(int[] unsorted, int left, int mid, int right)
+        {
+            int indexA = left;
+            int indexB = mid;
+            int tempIndex = 0;
+            int[] temp = new int[right];
+            while (indexA < mid && indexB < right)
+            {
+                if (unsorted[indexA] < unsorted[indexB])
+                    temp[tempIndex++] = unsorted[indexA++];
+                else
+                    temp[tempIndex++] = unsorted[indexB++];
+            }
+
+            while (indexA < mid)
+                temp[tempIndex++] = unsorted[indexA++];
+            while (indexB < right)
+                temp[tempIndex++] = unsorted[indexB++];
+
+            for (int i = 0; i < tempIndex; i++)
+                unsorted[i + left] = temp[i];
+        }
+    }
+}
