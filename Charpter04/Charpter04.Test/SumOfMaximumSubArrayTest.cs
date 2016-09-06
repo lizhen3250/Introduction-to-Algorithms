811ec7d0ccdca4b2b4fc111ea5d9a66ec1013e5e
lizhen3250 <764482832@qq.com>
2016-09-06 23:52:00 +0800

    Create Charpter 04 project and implemented Maximum sub array with O(n)

---
+++ Charpter04/Charpter04.Test/SumOfMaximumSubArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Charpter04;
+
+namespace Charpter04.Test
+{
+    [TestClass]
+    public class SumOfMaximumSubArrayTest
+    {
+        [TestMethod]
+        public void TestMethodSumOfMaximumSubArray()
+        {
+            int[] nums = { 1, 2, -1, -2, 4, 5, 6, 1 };
+            int res = SumOfMaximumArray.MaximumSubArray(nums);
+            Assert.AreEqual(16, res);
+        }
+    }
+}
---
+++ Charpter04/Charpter04/SumOfMaximumArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charpter04
+{
+    public class SumOfMaximumArray
+    {
+        public static int MaximumSubArray(int[] nums)
+        {
+            int max = int.MinValue;
+            int sum = int.MinValue;
+            for(int i = 0; i<nums.Length; i++)
+            {
+                if (sum > 0)
+                    sum += nums[i];
+                else
+                    sum = nums[i];
+                if (sum > max)
+                    max = sum;
+            }
+            return max;
+        }
+    }
+}
