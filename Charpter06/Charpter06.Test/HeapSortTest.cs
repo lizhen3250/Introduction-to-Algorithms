c09755ad2a9b59483cc1a52598d912bc5e1f5379
lizhen3250 <764482832@qq.com>
2016-09-07 22:39:06 +0800

    Implemented Heap Sort

---
+++ Charpter06/Charpter06.Test/HeapSortTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Charpter06;
+
+namespace Charpter06.Test
+{
+    [TestClass]
+    public class HeapSortTest
+    {
+        [TestMethod]
+        public void TestMethodMaxHeapify()
+        {
+            int[] nums = { 27, 16, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0};
+            HeapSort.MaxHeapify(nums, 2, nums.Length - 1);
+            CollectionAssert.AreEqual(nums, new int[] { 27, 16, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0 });
+        }
+
+        [TestMethod]
+        public void TestMethodBuildMaxHeap()
+        {
+            int[] nums = { 27, 16, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0 };
+            HeapSort.BuildMaxHeap(nums);
+            CollectionAssert.AreEqual(nums, new int[] { 27, 16, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0 });
+        }
+
+        [TestMethod]
+        public void TestMethodSort()
+        {
+            int[] nums = { 27, 16, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0 };
+            HeapSort.Sort(nums);
+            CollectionAssert.AreEqual(nums, new int[] { 0, 1, 3, 4, 5, 7, 8, 9, 10, 12, 13, 16, 16, 27 });
+        }
+    }
+}
---
+++ Charpter06/Charpter06/HeapSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charpter06
+{
+    public class HeapSort
+    {
+        public static void MaxHeapify(int[] nums, int i, int heapSize)
+        {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int largest = 0;
+
+            if (left < heapSize && nums[left] > nums[i])
+                largest = left;
+            else
+                largest = i;
+
+            if (right < heapSize && nums[right] > nums[largest])
+                largest = right;
+
+            if(largest != i)
+            {
+                int temp = nums[i];
+                nums[i] = nums[largest];
+                nums[largest] = temp;
+                MaxHeapify(nums, largest, heapSize);
+            }
+        }
+
+        public static void BuildMaxHeap(int[] nums)
+        {
+            for (int i = nums.Length/2 - 1; i >= 0; i--)
+                MaxHeapify(nums, i, nums.Length);
+        }
+
+        public static void Sort(int[] nums)
+        {
+            int heapSize = nums.Length;
+            BuildMaxHeap(nums);
+            for(int i = heapSize - 1; i>=1; i--)
+            {
+                int temp = nums[0];
+                nums[0] = nums[i];
+                nums[i] = temp;
+                heapSize--;
+                MaxHeapify(nums, 0, i);
+            }
+        }
+    }
+}
