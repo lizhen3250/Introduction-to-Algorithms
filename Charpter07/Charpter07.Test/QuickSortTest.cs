5190eb052ec9cd9c9a3428c7879d25f5e1651ff4
lizhen3250 <764482832@qq.com>
2016-09-11 22:00:59 +0800

    Implemented Quick Sort

---
+++ Charpter07/Charpter07.Test/QuickSortTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Charpter07;
+
+namespace Charpter07.Test
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void TestMethodQuickSort()
+        {
+            int[] nums = { 31, 46, 92, 48, 52, 2, 1, 4 };
+            QuickSort.Sort(nums, 0, nums.Length - 1);
+            foreach (var i in nums)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Charpter07/Charpter07/QuickSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charpter07
+{
+    public class QuickSort
+    {
+        public static int Partition(int[] nums, int start, int end)
+        {
+            int i = start - 1;
+            for(int j = start; j < end; j++)
+            {
+                if (nums[j] < nums[end])
+                {
+                    i++;
+                    Swap(ref nums[i], ref nums[j]);
+                }
+            }
+            Swap(ref nums[i + 1], ref nums[end]);
+            return i + 1;
+        }
+
+        public static void Sort(int[] nums, int start, int end)
+        {
+            if(start < end)
+            {
+                int mid = Partition(nums, start, end);
+                Sort(nums, start, mid - 1);
+                Sort(nums, mid + 1, end);
+            }
+        }
+
+        private static void Swap(ref int number1, ref int number2)
+        {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+    }
+}
