a54106dc1993baa4c286a3f3937bef40435fb6f9
lizhen3250 <764482832@qq.com>
2016-09-12 20:20:34 +0800

    Implemented Selection Sort

---
+++ Charpter08/Charpter08.Test/SelectionSortTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Charpter08;
+
+namespace Charpter08.Test
+{
+    [TestClass]
+    public class SelectionSortTest
+    {
+        [TestMethod]
+        public void TestMethodSelectionSort()
+        {
+            int[] arr = { 38, 32, -1, 90, 3, 2, 5, 9, 7 };
+            SelectionSort.Sort(ref arr);
+            foreach (var i in arr)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Charpter08/Charpter08/SelectionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charpter08
+{
+    public class SelectionSort
+    {
+        public static void Sort(ref int[] arr)
+        {
+            int pos = int.MinValue;
+
+            for(int i = 0; i<arr.Length; i++)
+            {
+                pos = i;
+                for(int j = i +1; j<arr.Length; j++)
+                {
+                    if (arr[pos] > arr[j])
+                        pos = j;
+                }
+                Swap(ref arr[i], ref arr[pos]);
+            }
+        }
+
+        private static void Swap(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+    }
+}
