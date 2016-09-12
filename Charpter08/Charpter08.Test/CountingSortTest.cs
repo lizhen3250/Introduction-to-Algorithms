d702e34b8283a9bbd0c30b131fad2a6137c42f92
lizhen3250 <764482832@qq.com>
2016-09-12 19:04:37 +0800

    Implemented Counting Sort

---
+++ Charpter08/Charpter08.Test/CountingSortTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Charpter08;
+
+namespace Charpter08.Test
+{
+    [TestClass]
+    public class CountingSortTest
+    {
+        [TestMethod]
+        public void TestMethodCountingSort()
+        {
+            int[] arr = { 2, 5, 6, 1, 3, 2, 4, 10, 50 };
+            int[] res = CountingSort.Sort(arr);
+            CollectionAssert.AreEqual(res, new int[] { 1, 2, 2, 3, 4, 5, 6, 10, 50 });
+        }
+    }
+}
---
+++ Charpter08/Charpter08/CountingSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Charpter08
+{
+    public class CountingSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            int[] sortedArray = new int[arr.Length];
+            int max = FindMaximumValue(arr);
+            int[] countArray = new int[max + 1];
+
+            for (int i = 0; i < arr.Length; i++)
+                countArray[arr[i]]++;
+
+            for (int i = 1; i < countArray.Length; i++)
+                countArray[i] = countArray[i] + countArray[i - 1];
+
+            for(int i = 0; i<arr.Length; i++)
+            {
+                sortedArray[countArray[arr[i]] - 1] = arr[i];
+                countArray[arr[i]]--;
+            }
+
+            return sortedArray;
+        }
+
+        private static int FindMaximumValue(int[] arr)
+        {
+            int max = int.MinValue;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            return max;
+        }
+    }
+}
